01cc2f04479d66cd23b106d8a379e37ca85100a9
Jonathan Finlay <jonathan@origin-digital.com>
2017-08-30 15:09:28 +0100

    Create bare project via lib

---
+++ StringExtensions/Classes/StringExtensions.swift
@@ -0,0 +1,14 @@
+import Foundation
+import UIKit
+
+public extension String {
+    
+    public var isEmail: Bool {
+        return matches(pattern: "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}")
+    }
+    func matches(pattern: String) -> Bool {
+        return range(of: pattern,
+                     options: String.CompareOptions.regularExpression,
+                     range: nil, locale: nil) != nil
+    }
+}
