6a80943f37e07af487fef1cb58ff78772484ea39
Balazs Szamody <bszamody@gmail.com>
2020-02-19 21:50:29 +1100

    Fixed names and paths

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "cocoatest",
+    name: "StringExtensions",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "cocoatest",
-            targets: ["cocoatest"]),
+            name: "StringExtensions",
+            targets: ["StringExtensions"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "cocoatest",
-            dependencies: []),
+            name: "StringExtensions",
+            dependencies: [],
+            path: "./StringExtensions/Classes"),
         .testTarget(
-            name: "cocoatestTests",
-            dependencies: ["cocoatest"]),
+            name: "StringExtensionsTests",
+            dependencies: ["StringExtensions"]),
     ]
 )
