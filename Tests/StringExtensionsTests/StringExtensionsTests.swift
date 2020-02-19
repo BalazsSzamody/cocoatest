b35c18be713fb22af0d9202667eb11588a4305af
Balazs Szamody <bszamody@gmail.com>
2020-02-19 21:51:50 +1100

    Fixed Test imports

--- Tests/StringExtensionsTests/StringExtensionsTests.swift
+++ Tests/StringExtensionsTests/StringExtensionsTests.swift
@@ -1,7 +1,7 @@
 import XCTest
-@testable import cocoatest
+@testable import StringExtensions
 
-final class cocoatestTests: XCTestCase {
+final class StringExtensionsTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
